936f18e6bbcac25c860cc63d4db533f29880101d
MichelNassarleb <michelnassar371@gmail.com>
2023-02-09 23:32:18 +0200

    Added text to the mockdata array items

--- src/mockData/mockData.ts
+++ src/mockData/mockData.ts
@@ -7,20 +7,26 @@ import fifthPic from '../assets/images/fifth.jpeg'
 
 export const mockData:Array<{name:string,
     type:string,
+    text:string,
     }> = [
     {name:firstPic,
     type:'COMMISSIONS',
+    text:'firstPic'
     },
      {name:secondPic,
     type:'PERSONNAL',
+    text:'firstPic'
     },
     {name:thirdPic,
         type:'FLORENCE',
+        text:'firstPic'
         },
     {name:fourthPic,
             type:'INFO',
+            text:'firstPic'
             },
     {name:fifthPic,
         type:'COMMISSIONS',
+        text:'firstPic'
     },
 ]

1ccc5440182df6a8657a8c0bad87820476ba6edc
MichelNassarleb <michelnassar371@gmail.com>
2023-02-09 22:59:09 +0200

    Created the RootNavigator and implemented it in the app file

---
+++ src/navigation/rootNavigator.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Provider } from "react-redux";
+import { Route, Routes } from "react-router";
+import { BrowserRouter } from "react-router-dom";
+import { store } from "../redux/store";
+import { HomeScreen } from "../screens/homeScreen";
+import { TypeScreen } from "../screens/typeScreen";
+
+export const RootNavigator = () =>{
+
+    return <Provider store={store}>
+    <BrowserRouter>
+    <Routes>
+        <Route path="/" element={<HomeScreen />}/>
+        <Route path="/type/:type" element={<TypeScreen />} />
+    </Routes>
+
+    </BrowserRouter>
+    </Provider>
+}

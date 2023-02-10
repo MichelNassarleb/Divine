6ad272f1ec07160fe7a881e9e266b5f227ec8dd2
MichelNassarleb <michelnassar371@gmail.com>
2023-02-10 23:45:33 +0200

    Implemented the snap list in the home screen

--- src/screens/homeScreen.tsx
+++ src/screens/homeScreen.tsx
@@ -1,17 +1,25 @@
-import React, { useEffect } from "react";
+import React, { useEffect, } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { SnapItem, SnapList } from "react-snaplist-carousel";
 import { Navbar } from "../components/navbar/navbar";
 import { mockData } from "../mockData/mockData";
 import { addTypesOfPictures } from "../redux/slices/appSlice";
-
-
+import { SnapImage } from "../components/imageItem/snapImage";
+import './homeScreenStyles.css'
 export const HomeScreen = () =>{
     const dispatch = useDispatch()
   useEffect(()=>{
     dispatch(addTypesOfPictures(mockData))
   },[])
  const types = useSelector((state:any)=>state.app.typesOfPictures)
-  return <>
+
+  return <main style={{height:'100vh',overflow:'hidden'}}>
   <Navbar name="Divine Gerges" types={types} copyright="© Divine Gerges 2023"/>
-  </>
+              <SnapList direction="horizontal" className="snaplist-container">
+                       <SnapItem snapAlign="center" width="300px" className="empty-div"></SnapItem>
+                        {mockData.map((image, index) =>
+                            <SnapImage image={image} key={index}/>
+                        )}
+                    </SnapList>
+  </main>
 }

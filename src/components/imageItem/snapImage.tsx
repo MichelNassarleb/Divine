450e095244ca68bb1bba34fa93b8c088e3c38c74
MichelNassarleb <michelnassar371@gmail.com>
2023-02-12 18:29:29 +0200

    Added the styling of the info btn

--- src/components/imageItem/snapImage.tsx
+++ src/components/imageItem/snapImage.tsx
@@ -1,19 +1,46 @@
-import React,{FC} from "react";
+import React, { FC, useState } from "react";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import { SnapItem } from "react-snaplist-carousel";
 import { SnapImageProps } from "../../config/interfaces";
-import './snapImageStyles.css'
-import CloseIcon from '@mui/icons-material/Close';
-
-export const SnapImage:FC<SnapImageProps> = (props:any) =>{
-  
-    return ( <SnapItem margin={{ left: '0px', right: '0px' }} className='snap-item' height={'100%'} width={'100%'} snapAlign="center" key={props?.key}>
-    <LazyLoadImage effect="blur" src={props?.image.name}  className='img-carousel' />
-    <p className="first-subtitle">{props?.image?.text}</p>
-    <div className="second-subtitle">
-        <p >{props?.image?.text}</p>
-        {/* <button>Info</button> */}
-        <CloseIcon style={{color:'grey'}}/>
-    </div>
-</SnapItem>)
-}
+import "./snapImageStyles.css";
+import CloseIcon from "@mui/icons-material/Close";
+import { motion } from "framer-motion";
+export const SnapImage: FC<SnapImageProps> = (props: any) => {
+  const [infoDisplayed, setInfoDisplayed] = useState(false);
+  return (
+    <SnapItem
+      margin={{ left: "0px", right: "0px" }}
+      className="snap-item"
+      height={"100%"}
+      width={"100%"}
+      snapAlign="center"
+      key={props?.key}
+    >
+      <LazyLoadImage
+        effect="blur"
+        src={props?.image.name}
+        className="img-carousel"
+      />
+      <p className="first-subtitle">{props?.image?.text}</p>
+      {infoDisplayed ? (
+        <motion.div
+          initial={{ opacity: 0, height: 0 }}
+          animate={{ opacity: 1, height: "100%" }}
+          className="second-subtitle"
+        >
+          <p>{props?.image?.text}</p>
+          <CloseIcon
+            style={{ color: "grey",cursor:'pointer' }}
+            onClick={() => setInfoDisplayed((prev) => !prev)}
+          />
+        </motion.div>
+      ) : (
+        <div className="second-subtitle info-btn">
+          <motion.button initial={{opacity:0}} animate={{opacity:1}} onClick={() => setInfoDisplayed((prev) => !prev)}>
+            Info
+          </motion.button>
+        </div>
+      )}
+    </SnapItem>
+  );
+};

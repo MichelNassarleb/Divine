877e244c80dbd3e8f0dbb054323fac69c11a6f71
MichelNassarleb <michelnassar371@gmail.com>
2023-02-15 22:50:06 +0200

    Changed the color of the div

--- src/components/loader/loader.tsx
+++ src/components/loader/loader.tsx
@@ -4,6 +4,6 @@ import './loaderStyles.css'
 
 export const Loader:FC<{isLoading?:boolean}> = (props) =>{
     return<div className="loader-container" style={props?.isLoading ? {zIndex:100} : {display:'none'}}>
-        <CircleLoader color="grey" />
+        <CircleLoader color="#fff" />
     </div>
 }

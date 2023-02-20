c51ddcba9e68ac32d373e7fcb28934f10bbb43f4
MichelNassarleb <michelnassar371@gmail.com>
2023-02-20 22:42:03 +0200

    updated the interface of the loader props to support color externalized props

--- src/components/loader/loader.tsx
+++ src/components/loader/loader.tsx
@@ -2,8 +2,9 @@ import React, { FC } from "react";
 import { CircleLoader } from "react-spinners";
 import './loaderStyles.css'
 
-export const Loader:FC<{isLoading?:boolean}> = (props) =>{
+export const Loader:FC<{isLoading?:boolean,color?:string}> = (props) =>{
+
     return<div className="loader-container" style={props?.isLoading ? {zIndex:100} : {display:'none'}}>
-        <CircleLoader color="#fff" />
+        <CircleLoader />
     </div>
 }

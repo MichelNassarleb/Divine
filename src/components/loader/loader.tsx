e9653c08ce8e8e8e116f6d315212db18d45290b5
MichelNassarleb <michelnassar371@gmail.com>
2023-02-24 17:27:44 +0200

    Fixed loader being displayed behind the navbar

--- src/components/loader/loader.tsx
+++ src/components/loader/loader.tsx
@@ -4,7 +4,7 @@ import './loaderStyles.css'
 
 export const Loader:FC<{isLoading?:boolean,color?:string}> = (props) =>{
 
-    return props?.isLoading ? <div role={'dialog'} className="loader-container">
+    return props?.isLoading ? <div role={'dialog'} className="loader-container" style={props?.isLoading ? {zIndex:100} : {display:'none'}}>
         <CircleLoader color={props?.color}/>
-    </div> : null
+    </div> :null
 }

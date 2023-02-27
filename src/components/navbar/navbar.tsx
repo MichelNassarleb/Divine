ac2d085282adf8bf0de6a23960fe95c93d84d2cb
MichelnassarLeb <michelnassar371@gmail.com>
2023-02-27 22:27:12 +0200

    Updated the navbar component with roles

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -8,11 +8,11 @@ export const Navbar:FC<NavbarProps> = (props) =>{
     
     return <nav className="navbar-container">
         <div className="navbar-subcontainer">
-        {props?.name ? <h1 className="navbar-name"><a href="/"> {props.name}</a></h1> : null}
+        {props?.name ? <h1 className="navbar-name" role={'definition'}><a href="/"> {props.name}</a></h1> : null}
         <div className="navbar-types-container">
         {props?.types?.length > 0 ? props.types.map((item:string,index)=><Link key={index} to={`/category/${item}`} className="navbar-type"  children={item}/>) : null}
         </div>
-        {props?.copyright ? <p className="copyright">{props.copyright}</p> : null}
+        {props?.copyright ? <p role={'article'} className="copyright">{props.copyright}</p> : null}
         <a target={'_blank'} href='https://www.instagram.com/divinegrgs/?igshid=Yzg5MTU1MDY=' className="navbar-type">
         <InstagramIcon fontSize="small"/>
         </a>

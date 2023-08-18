77649d12882a82c43465967218a53b61cc50e1e9
MichelnassarLeb <michelnassar371@gmail.com>
2023-08-18 20:28:18 +0300

    feat: added lottie animation for loading screen

--- src/components/loader/loader.tsx
+++ src/components/loader/loader.tsx
@@ -1,10 +1,14 @@
 import React, { FC } from "react";
 import { CircleLoader } from "react-spinners";
 import './loaderStyles.css'
+import Lottie from "lottie-react";
+import loadingAnimation from '../../assets/animations/animation_llgutygb.json'
 
 export const Loader:FC<{isLoading?:boolean,color?:string}> = (props) =>{
 
     return props?.isLoading ? <div role={'dialog'} className="loader-container" style={props?.isLoading ? {zIndex:100} : {display:'none'}}>
-        <CircleLoader color={props?.color}/>
+        <div className="loader-children">
+        <Lottie animationData={loadingAnimation} loop={true} width={50} height={50}/>;
+        </div>
     </div> :null
 }

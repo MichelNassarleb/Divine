baba97ba62617f2e3406b7dacd8d2976708537bd
MichelnassarLeb <michelnassar371@gmail.com>
2023-08-18 13:45:40 +0300

    fix: fixed error in tests

--- src/tests/components/snapImage.test.tsx
+++ src/tests/components/snapImage.test.tsx
@@ -7,16 +7,16 @@ describe('<SnapImage />',()=>{
         expect(screen.queryByRole('img')).toBeFalsy()
     })
     it('if theres  src path for the image, component should render',()=>{
-        render(<SnapImage image={{name:'./asf',}}/>)
+        render(<SnapImage image={{uri:'./asf',}}/>)
         expect(screen.queryByRole('img')).toBeTruthy()
     })
     it('if theres no image text, text should not show',()=>{
-        render(<SnapImage image={{name:'./asf',}}/>)
+        render(<SnapImage image={{uri:'./asf',}}/>)
         expect(screen.queryByRole('paragraph')).toBeFalsy()
         expect(screen.queryByRole('img')).toBeTruthy()
     })
     it('if theres image text, text should show',()=>{
-        render(<SnapImage image={{name:'./asf',text:'test'}}/>)
+        render(<SnapImage image={{uri:'./asf',text:'test'}}/>)
         expect(screen.queryByText('test')).toBeTruthy()
         expect(screen.queryByRole('img')).toBeTruthy()
     })

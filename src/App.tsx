6713715bee3910d22020c64a7967155243a624c6
MichelnassarLeb <michelnassar371@gmail.com>
2023-08-18 13:45:03 +0300

    feat: initialized firebase

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 
 import './App.css';
 import { RootNavigator } from './navigation/rootNavigator';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { firebaseConfig } from './config/firebaseConfig';
+import {getFirestore} from 'firebase/firestore'
 
 function App() {
   
@@ -11,3 +16,9 @@ function App() {
 }
 
 export default App;
+
+
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const db = getFirestore(app)
